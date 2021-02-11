dc7725aff7fd92aa8da48ea168f48dec14a15fe0
Roger.OSullivan <Roger.OSullivan@BDX-LDN-LT26.bordeaux.local>
2021-02-11 17:22:28 +0000

    Second Maven Project

---
+++ src/test/java/MavenTests/AttemptTest/SimpleTestSuite.java
@@ -0,0 +1,80 @@
+package MavenTests.AttemptTest;
+
+
+
+
+
+import static org.junit.Assert.assertEquals;
+
+//import static org.testng.Assert.assertEquals;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+
+
+public class SimpleTestSuite {
+
+	
+
+	
+	
+	
+	
+	@After
+	public void after()
+	{
+		System.out.println("This is after Annotation");
+	}
+
+	
+	@Test
+	public void display()
+	{
+		System.out.println("This is Display Method");
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://uat.bordeauxindex.com/fine-wine/red-bordeaux/margaux/palmer-2016");
+	    System.out.println(driver.getTitle());
+		assertEquals(driver.getTitle(), "2016 Palmer");
+		driver.close();
+
+	}
+	
+	
+	@Test
+	public void test2()
+	{
+		System.out.println("This is test 2");
+
+	}
+	
+	
+	@Test
+	public void test3()
+	{
+		System.out.println("This is test 3");
+		System.out.println("This is Display Method");
+//		WebDriver driver = new ChromeDriver();
+	//	driver.get("https://www.google.com");
+	//	System.out.println(driver.getTitle());
+		//assertEquals(driver.getTitle(), "Google");
+		//driver.close();
+
+	}
+	
+	
+	@Before
+	public void testing()
+	{
+		System.out.println("This is before Annotation");
+	}
+
+}
